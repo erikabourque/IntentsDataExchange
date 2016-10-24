db2e1025f532df3483368bd85e85f1980ed22043
Erika Bourque <erika.bourque@dawsoncollege.qc.ca>
2016-10-24 15:10:15 -0400

    Completed Image Activity

--- app/src/main/java/com/example/a1141669/intentsdataexchange/ImageActivity.java
+++ app/src/main/java/com/example/a1141669/intentsdataexchange/ImageActivity.java
@@ -1,13 +1,47 @@
 package com.example.a1141669.intentsdataexchange;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ImageActivity extends AppCompatActivity {
+    static final int INPUT_REQUEST = 1;
+    static final int DAVE_REQUEST = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+    }
+
+    public void launchInputActivity(View v)
+    {
+        Intent intent = new Intent(this, InputActivity.class);
+
+        startActivityForResult(intent, INPUT_REQUEST);
+    }
+
+    public void launchDaveActivity(View v)
+    {
+        Intent intent = new Intent(this, DaveActivity.class);
+
+        startActivityForResult(intent, DAVE_REQUEST);
+    }
+
+    protected void onActivityResult(int request, int resultCode, Intent data)
+    {
+        // Both do same thing, check for either one
+        if ((request == INPUT_REQUEST) || (request == DAVE_REQUEST))
+        {
+            // Make sure there is a result
+            if (resultCode == RESULT_OK)
+            {
+                // Send result back to calling activity
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/a1141669/intentsdataexchange/InputActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1141669.intentsdataexchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}

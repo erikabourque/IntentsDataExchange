7b1a91ec70e6fd8c582815b4a8cfe88439cc346b
Erika Bourque <erika.bourque@dawsoncollege.qc.ca>
2016-10-24 14:56:32 -0400

    Completed CountActivity

--- app/src/main/java/com/example/a1141669/intentsdataexchange/CountActivity.java
+++ app/src/main/java/com/example/a1141669/intentsdataexchange/CountActivity.java
@@ -1,7 +1,9 @@
 package com.example.a1141669.intentsdataexchange;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CountActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class CountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count);
+    }
+
+    public void returnOne(View v)
+    {
+        Intent intent = new Intent();
+        intent.putExtra("result", getResources().getString(R.string.oneStr));
+
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    public void returnTwo(View v)
+    {
+        Intent intent = new Intent();
+        intent.putExtra("result", getResources().getString(R.string.twoStr));
+
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
